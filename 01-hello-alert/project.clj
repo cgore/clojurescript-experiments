4af9def51015ff154f25e235abb535efe1f4c97d
Christopher Mark Gore <cgore@cgore.com>
2014-03-23 22:37:11 -0500

    Initial commit: basic working ClojureScript app.

---
+++ 01-hello-alert/project.clj
@@ -0,0 +1,21 @@
+(defproject hello-alert "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2156"]
+                 [ring "1.2.1"]]
+  :plugins [[lein-cljsbuild "1.0.2"]
+            [lein-ring "0.8.10"]]
+  :hooks [leiningen.cljsbuild]
+  :source-paths ["src/clj"]
+  :cljsbuild { 
+    :builds {
+      :main {
+        :source-paths ["src/cljs"]
+        :compiler {:output-to "resources/public/js/cljs.js"
+                   :optimizations :simple
+                   :pretty-print true}
+        :jar true}}}
+  :main hello-alert.server
+  :ring {:handler hello-alert.server/app})
+
