a7008fedadc0046714d0a85f22cb8de59d918bc7
Christopher Mark Gore <cgore@cgore.com>
2014-03-25 23:58:44 -0500

    Adding all clock hands.

--- 03-analog-clock/src/cljs/analog_clock/client.cljs
+++ 03-analog-clock/src/cljs/analog_clock/client.cljs
@@ -6,19 +6,83 @@
   (dom/set-text (dom/get-element "digital-clock")
                 (js/Date)))
 
-(defn draw-line [context start-point end-point color line-width]
+(def pi (aget js/Math "PI"))
+(def tau (* 2 pi))
+
+(defn radians-from-degrees [degrees]
+  (* tau (/ (mod degrees 360) 360)))
+
+(defn sin [theta]
+  (.sin js/Math theta))
+(defn cos [theta]
+  (.cos js/Math theta))
+(defn tan [theta]
+  (.tan js/Math theta))
+
+(defn cartesian-from-polar [[x0 y0] r theta]
+  [(+ x0 (* r (cos theta)))
+   (+ y0 (* r (sin theta)))])
+
+(defn draw-line [context [x0 y0] [x1 y1] color line-width]
   (set! (. context -strokeStyle) color)
   (set! (. context -lineCap) "round")
   (set! (. context -lineWidth) line-width)
   (.beginPath context)
-  (.moveTo context (first start-point) (second start-point))
-  (.lineTo context (first end-point) (second end-point))
+  (.moveTo context x0 y0)
+  (.lineTo context x1 y1)
   (.stroke context))
+
+(defn degrees-from-hour [hour]
+  (let [degrees-per-hour (/ 360 12)
+        hour-offset 3]
+    (* (- hour hour-offset) degrees-per-hour)))
+
+(defn radians-from-hour [hour]
+  (radians-from-degrees (degrees-from-hour hour)))
+
+(defn degrees-from-minute [minute]
+  (let [degrees-per-minute (/ 360 60)
+        minute-offset 15]
+    (* (- minute minute-offset)
+       degrees-per-minute)))
+
+(defn radians-from-minute [minute]
+  (radians-from-degrees (degrees-from-minute minute)))
+
+(defn radians-from-second [second]
+  (radians-from-minute second))
+
+(defn draw-hour-hand [context center hour]
+  (draw-line context
+             center
+             (cartesian-from-polar center 100 (radians-from-hour hour))
+             "red"
+             20))
+
+(defn draw-minute-hand [context center minute]
+  (draw-line context
+             center
+             (cartesian-from-polar center 150 (radians-from-minute minute))
+             "green"
+             10))
+
+(defn draw-second-hand [context center second]
+  (draw-line context
+             center
+             (cartesian-from-polar center 175 (radians-from-second second))
+             "blue"
+             5))
 
 (defn update-analog-clock []
   (let [canvas (dom/get-element "analog-clock")
-        context (.getContext canvas "2d")]
-    (draw-line context [200 200] [300 300] "red" 20)))
+        context (.getContext canvas "2d")
+        width 400
+        height 400
+        center [(/ width 2)
+                (/ height 2)]]
+    (draw-hour-hand context center 12)
+    (draw-minute-hand context center 17)
+    (draw-second-hand context center 33)))
 
 (defn update-clock []
   (update-digital-clock)
