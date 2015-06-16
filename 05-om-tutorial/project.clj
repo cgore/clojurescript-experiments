f02e43a1870eb42018a635b5212b386dabcc3f18
Christopher Mark Gore <cgore@cgore.com>
2015-06-15 19:47:18 -0500

    I built the om tutorial wrong; retry.

--- 05-om-tutorial/project.clj
+++ 05-om-tutorial/project.clj
@@ -1,10 +1,14 @@
-(defproject om "0.1.0-SNAPSHOT"
+(defproject om-tutorial "0.1.0-SNAPSHOT"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-3211"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [sablono "0.3.4"]
+                 [org.omcljs/om "0.8.8"]]
 
   :plugins [[lein-cljsbuild "1.0.5"]
             [lein-figwheel "0.3.3"]]
@@ -17,17 +21,17 @@
     :builds [{:id "dev"
               :source-paths ["src"]
 
-              :figwheel { :on-jsload "om.core/on-js-reload" }
+              :figwheel { :on-jsload "om-tutorial.core/on-js-reload" }
 
-              :compiler {:main om.core
+              :compiler {:main om-tutorial.core
                          :asset-path "js/compiled/out"
-                         :output-to "resources/public/js/compiled/om.js"
+                         :output-to "resources/public/js/compiled/om_tutorial.js"
                          :output-dir "resources/public/js/compiled/out"
                          :source-map-timestamp true }}
              {:id "min"
               :source-paths ["src"]
-              :compiler {:output-to "resources/public/js/compiled/om.js"
-                         :main om.core                         
+              :compiler {:output-to "resources/public/js/compiled/om_tutorial.js"
+                         :main om-tutorial.core                         
                          :optimizations :advanced
                          :pretty-print false}}]}
 
--- 05-om-tutorial/src/om_tutorial/core.cljs
+++ 05-om-tutorial/src/om_tutorial/core.cljs
@@ -1,5 +1,6 @@
-(ns ^:figwheel-always om.core
-    (:require))
+(ns ^:figwheel-always om-tutorial.core
+    (:require[om.core :as om :include-macros true]
+              [om.dom :as dom :include-macros true]))
 
 (enable-console-print!)
 
@@ -8,6 +9,14 @@
 ;; define your app data so that it doesn't get over-written on reload
 
 (defonce app-state (atom {:text "Hello world!"}))
+
+(om/root
+  (fn [data owner]
+    (reify om/IRender
+      (render [_]
+        (dom/h1 nil (:text data)))))
+  app-state
+  {:target (. js/document (getElementById "app"))})
 
 
 (defn on-js-reload []
